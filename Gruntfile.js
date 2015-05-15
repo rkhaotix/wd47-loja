424f1935ccc0e759909574a05264c6de4713d531
Raphael A. Silva <rkhaotix@gmail.com>
2015-05-15 16:39:52 -0300

    Exemplos de uso do Grunt para minificar, ofuscar js e css

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,24 @@ module.exports = function(grunt) {
 			}
 		},
 		
-		clean: ['dist']
+		clean: ['dist'],
+		
+		useminPrepare: {
+			html: ['dist/**/*.html']
+		},
+		
+		usemin: {
+			html: ['dist/**/*.html']
+		}
 	});
 	
-	grunt.registerTask('default', ['clean','copy']);
+	grunt.registerTask('minifica', ['useminPrepare','usemin', 'concat','uglify','cssmin']);
+	grunt.registerTask('default', ['clean','copy','minifica']);
+	
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-usemin');
 }
